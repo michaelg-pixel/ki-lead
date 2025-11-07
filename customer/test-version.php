389067c65cdd43fdd873cfdc0585353bc8d37e4f
michaelg-pixel <michael.gluska@gmail.com>
2025-11-07 10:36:17 +0100

    Add test file to check if updates are working

---
+++ customer/test-version.php
@@ -0,0 +1,6 @@
+<?php
+// TEST: Zeigt ob die neue Version geladen wird
+echo "VERSION: NEUE VERSION MIT MULTI-VIDEO SUPPORT - " . date('H:i:s');
+echo "<br><br>";
+echo "<a href='course-view.php?id=5'>→ Zum Kurs</a>";
+?>
