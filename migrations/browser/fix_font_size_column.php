80c8001e59890c1de3ff4b037a824dbf8c943144
michaelg-pixel <michael.gluska@gmail.com>
2025-11-11 14:25:14 +0100

    Fix: Korrekter Pfad zur database.php

--- migrations/browser/fix_font_size_column.php
+++ migrations/browser/fix_font_size_column.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../../config/database.php';
 
 // Nur für Admins
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
