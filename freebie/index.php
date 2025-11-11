d67af404dff87e911d7c719126807081215a0335
michaelg-pixel <michael.gluska@gmail.com>
2025-11-11 12:45:59 +0100

    Delete corrupted file before restore

--- freebie/index.php
+++
@@ -1,8 +0,0 @@
-        /* Mockup */
-        .mockup img {
-            max-width: 100%;
-            width: 100%;
-            max-width: 265px;
-            height: auto;
-            border-radius: 8px;
-        }
