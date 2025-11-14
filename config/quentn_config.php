04b88563d576638f79f35b50e645d41153960ffe
michaelg-pixel <michael.gluska@gmail.com>
2025-11-14 08:49:20 +0100

    Add Quentn API configuration for password reset emails

---
+++ config/quentn_config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Quentn API Konfiguration
+ * Für E-Mail-Versand (z.B. Passwort-Reset)
+ */
+
+define('QUENTN_API_BASE_URL', 'https://pk1bh1.eu-1.quentn.com/public/api/v1/');
+define('QUENTN_API_KEY', 'm-gkCLAXFVewwguCP1ZCm9zFFi_bauieZPl21EkGUqo');
+
+// E-Mail Absender-Einstellungen
+define('QUENTN_FROM_EMAIL', 'noreply@mehr-infos-jetzt.de');
+define('QUENTN_FROM_NAME', 'Optinpilot');
