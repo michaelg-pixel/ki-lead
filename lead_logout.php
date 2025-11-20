757e18273f69c7b34ed181135cdebf7dcf92ea38
michaelg-pixel <michael.gluska@gmail.com>
2025-11-20 12:58:49 +0100

    Fix: Session-Fehler beheben - vollständige Session-Bereinigung beim Logout

--- lead_logout.php
+++ lead_logout.php
@@ -2,13 +2,25 @@
 /**
  * Lead Logout
  * Leitet zurück zu lead_register.php mit freebie & customer Parametern
+ * FIXED: Vollständige Session-Bereinigung
  */
 
-session_start();
+// Session nur starten wenn nicht bereits aktiv
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-// Parameter aus Session holen
+// Parameter aus Session holen BEVOR wir sie zerstören
 $freebie_id = $_SESSION['lead_freebie_id'] ?? null;
 $customer_id = $_SESSION['lead_customer_id'] ?? null;
+
+// Session vollständig bereinigen
+$_SESSION = array(); // Alle Session-Variablen löschen
+
+// Session-Cookie löschen
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
 
 // Session zerstören
 session_destroy();
